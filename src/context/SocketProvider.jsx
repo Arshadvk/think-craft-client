2712132e42d65c20fd40d91eca9a22eafc6733f8
Arshadvk <arshadvk7560@gmail.com>
2023-09-19 18:26:32 +0530

    SocketProvider

--- src/context/SocketProvider.jsx
+++ src/context/SocketProvider.jsx
@@ -1,29 +1,29 @@
-// import React, { createContext, useMemo, useContext } from "react";
-// import { io } from "socket.io-client";
+import React, { createContext, useMemo, useContext } from "react";
+import { io } from "socket.io-client";
 
-// const ENDPOINT = "https://egoft.shop";
-// // const ENDPOINT = 'http://localhost:5000'
+const ENDPOINT = "https://egoft.shop";
+// const ENDPOINT = 'http://localhost:5000'
 
-// const SocketContext = createContext(null);
+const SocketContext = createContext(null);
+
+export const useSocket = () => {
+  const socket = io(ENDPOINT);
+  return socket;
+};
 
 // export const useSocket = () => {
-//   const socket = io(ENDPOINT);
+//   const socket = useContext(SocketContext);
 //   return socket;
 // };
 
-// // export const useSocket = () => {
-// //   const socket = useContext(SocketContext);
-// //   return socket;
-// // };
+// const socket = io('http://localhost:4000');
 
-// // const socket = io('http://localhost:4000');
+const ContextProvider = ({ children }) => {
+  const socket = useMemo(() => io(ENDPOINT), []);
 
-// const ContextProvider = ({ children }) => {
-//   const socket = useMemo(() => io(ENDPOINT), []);
+  return (
+    <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
+  );
+};
 
-//   return (
-//     <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
-//   );
-// };
-
-// export { ContextProvider, SocketContext };
+export { ContextProvider, SocketContext };
