ae5c057505f9947d5dcb7a21a9aa88b0afbd6ee0
Arshadvk <arshadvk7560@gmail.com>
2023-09-19 14:52:55 +0530

    api updates

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,4 +1,9 @@
-export const studentAPI = "http://localhost:4000/"
-export const adminAPI = "http://localhost:4000/admin/"
-export const reviewerAPI = "http://localhost:4000/reviewer/"
-export const advisorAPI = "http://localhost:4000/advisor/"
+export const studentAPI = "https://think-craft-server.onrender.com/"
+export const adminAPI = "https://think-craft-server.onrender.com/admin/"
+export const reviewerAPI = "https://think-craft-server.onrender.com/reviewer/"
+export const advisorAPI = "https://think-craft-server.onrender.com/advisor/"
+
+// export const studentAPI = "http://localhost:4000/"
+// export const adminAPI = "http://localhost:4000/admin/"
+// export const reviewerAPI = "http://localhost:4000/reviewer/"
+// export const advisorAPI = "http://localhost:4000/advisor/"
