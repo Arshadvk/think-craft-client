d87f464efe3f348b54f93cb239983fd6948477bc
Arshadvk <arshadvk7560@gmail.com>
2023-09-19 16:11:00 +0530

    ..

--- src/constants/api.js
+++ src/constants/api.js
@@ -1,9 +1,15 @@
-export const studentAPI = "https://think-craft-server.onrender.com/"
-export const adminAPI = "https://think-craft-server.onrender.com/admin/"
-export const reviewerAPI = "https://think-craft-server.onrender.com/reviewer/"
-export const advisorAPI = "https://think-craft-server.onrender.com/advisor/"
+// export const studentAPI = "https://think-craft-server.onrender.com/"
+// export const adminAPI = "https://think-craft-server.onrender.com/admin/"
+// export const reviewerAPI = "https://think-craft-server.onrender.com/reviewer/"
+// export const advisorAPI = "https://think-craft-server.onrender.com/advisor/"
 
 // export const studentAPI = "http://localhost:4000/"
 // export const adminAPI = "http://localhost:4000/admin/"
 // export const reviewerAPI = "http://localhost:4000/reviewer/"
-// export const advisorAPI = "http://localhost:4000/advisor/"
+// export const advisorAPI = "http://localhost:4000/advisor/" 
+
+
+export const studentAPI = "https://egoft.shop/"
+export const adminAPI = "https://egoft.shop/admin/"
+export const reviewerAPI = "https://egoft.shop/reviewer/"
+export const advisorAPI = "https://egoft.shop/advisor/"
