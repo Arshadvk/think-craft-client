74e89bc5d696c3f7623e2d7dc3383e6ea4e90c72
Arshadvk <arshadvk7560@gmail.com>
2023-09-19 15:26:10 +0530

    socket

--- src/context/SocketProvider.jsx
+++ src/context/SocketProvider.jsx
@@ -1,19 +1,39 @@
-import React , {createContext , useMemo , useContext} from 'react'
-import {io} from 'socket.io-client'
+import React, { createContext, useMemo, useContext } from "react";
+import { io } from 'socket.io-client';
 
-const SocketContext  = createContext(null)
 
-export const useSocket = () =>{
-    const socket = useContext(SocketContext)
-    return socket ;
+
+
+
+const ENDPOINT = 'https://ego-ft.onrender.com'
+// const ENDPOINT = 'http://localhost:5000'
+
+
+const SocketContext = createContext<any>(null);
+
+// export const useSocket = () => {
+//   const socket = io(ENDPOINT)
+//   return socket;
+// };
+
+export const useSocket = () => {
+  const socket = useContext(SocketContext);
+  return socket;
+};
+
+// const socket = io('http://localhost:4000');
+
+
+const ContextProvider = ({ children }) => {
+  const socket = useMemo(() => io(ENDPOINT), []);
+
+  return (
+    <SocketContext.Provider value={socket}>
+      {children}
+    </SocketContext.Provider>
+  );
 }
 
-export const SocketProvider = (props) =>{
-    const socket = useMemo(()=> io("localhost:4000") , [])
 
-    return (
-        <SocketContext.Provider value={socket} >
-            {props.children}
-        </SocketContext.Provider>
-    )
-}
+
+export { ContextProvider, SocketContext };
